d137f944893171083768a52c54f37fb6851fbb60
begemot <m1233danil@bk.ru>
2025-01-14 13:28:07 +0300

    dv done

--- Lab4/dsDvoiniki.c
+++ Lab4/dsDvoiniki.c
@@ -16,7 +16,7 @@ typedef struct Allocator {
     size_t bitmap_size;
 } Allocator;
 
-#define MIN_BLOCK_SIZE 16  // Минимальный размер блока
+#define MIN_BLOCK_SIZE 16
 
 void printBits(uint8_t num) {
     for (int i = 7; i >= 0; i--) {
@@ -25,8 +25,7 @@ void printBits(uint8_t num) {
     printf("|");
 }
 
-// Функция для вычисления степени двойки, большей или равной size
-static size_t next_power_of_two(size_t size) {
+size_t next_power_of_two(size_t size) {
     size_t power = 1;
     while (power < size) {
         power <<= 1;
@@ -34,8 +33,7 @@ static size_t next_power_of_two(size_t size) {
     return power;
 }
 
-// Функция для вычисления индекса блока в битовой карте
-static size_t get_block_index(size_t block_size, size_t total_size, size_t offset) {
+size_t get_block_index(size_t block_size, size_t total_size, size_t offset) {
     return (offset / block_size) + (total_size / block_size) - 1;
 }
 
@@ -45,8 +43,8 @@ Allocator* allocator_create(void *const memory, const size_t size) {
     }
 
     Allocator *allocator = (Allocator *)memory;
+    printf("start addr: %p\n", memory);
     allocator->memory = (uint8_t *)memory + sizeof(Allocator);
-    printf("Allocator memroy at %p\n", allocator->memory);
     allocator->size = size - sizeof(Allocator);
 
     // Вычисляем размер битовой карты
@@ -62,23 +60,22 @@ Allocator* allocator_create(void *const memory, const size_t size) {
     allocator->bitmap = (uint8_t *)allocator->memory;
     allocator->memory = (uint8_t *)allocator->memory + allocator->bitmap_size;
     allocator->size -= allocator->bitmap_size;
+    printf("valid addr: %p\n", allocator->memory);
     printf("allocator size: %lu\n", allocator->size);
 
-    // Инициализируем битовую карту нулями
     memset(allocator->bitmap, 0, allocator->bitmap_size);
 
     return allocator;
 }
 
 void allocator_destroy(Allocator *const allocator) {
-    // Ничего не делаем, так как память управляется внешне
 }
 
 void* allocator_alloc(Allocator *const allocator, const size_t size) {
-    printf("Bitmap: ");
-    printBits(allocator->bitmap[0]);
-    printBits(allocator->bitmap[1]);
-    printf("\n");
+    //printf("Bitmap: ");
+    //printBits(allocator->bitmap[0]);
+    //printBits(allocator->bitmap[1]);
+    //printf("\n");
     if (size == 0 || size > allocator->size) {
         return NULL;
     }
@@ -106,12 +103,12 @@ void* allocator_alloc(Allocator *const allocator, const size_t size) {
                 for (int i = 0; i < checkCnt; i++) {
                     size_t ind = index + i;
                     printf("check index: %lu\n", ind);
-                    // printf("byte: ");
-                    // printBits(allocator->bitmap[ind / 8]);
-                    // printf("  mask: ");
-                    // printBits((1 << (7 - (ind % 8))));
-                    // printf("\n");
-                    // printf("Res: %d\n", (allocator->bitmap[ind / 8] & (1 << (7 - (ind % 8)))));
+                     printf("byte: ");
+                     printBits(allocator->bitmap[ind / 8]);
+                     printf("  mask: ");
+                     printBits((1 << (7 - (ind % 8))));
+                     printf("\n");
+                     printf("Res: %d\n", (allocator->bitmap[ind / 8] & (1 << (7 - (ind % 8)))));
                     if ((allocator->bitmap[ind / 8] & (1 << (7 - (ind % 8)))) != 0) {
                         isFree = 0;
                         // printf("ahahahahaha\n");
@@ -129,7 +126,7 @@ void* allocator_alloc(Allocator *const allocator, const size_t size) {
                 index = originIndex;
                 while (index > 0) {
                     index = index / 2;
-                    printf("Check index: %lu\n", index);
+                    //printf("Check index: %lu\n", index);
                     if ((allocator->bitmap[index / 8] & (1 << (7 - (index % 8)))) != 0) {
                         isFree = 0;
                         break;
@@ -144,47 +141,35 @@ void* allocator_alloc(Allocator *const allocator, const size_t size) {
             printf("Offset: %lu.  ", offset);
             printf("Addr: <%p>\n", (uint8_t *)allocator->memory + offset);
             printf("----ALLOC ENDED----\n\n");
-            return (uint8_t *)allocator->memory + offset;
+            return (void*)((uint8_t *)allocator->memory + offset);
         }
         offset += block_size;
     }
     return NULL;  // Не удалось найти свободный блок
 }
 
-void allocator_free(Allocator *const allocator, void* memory) {
+void allocator_free(Allocator *const allocator, void* const memory) {
     printf("\n##### FREE STARTED #####\n");
     if (memory == NULL) {
         printf("Memory null\n");
         return;
     }
-    printf("Mem not null\n");
-
-    printf("Mem start: %d\n", allocator->memory);
-    printf("Mem start: %p\n", allocator->memory);
-
-    //uint8_t* memStart = (uint8_t*)allocator->memory;
-    //printf("Mem start: <%p>", memStart);
-
-    printf("Memory addr: <%d>", memory);
-    printf("Memory addr: <%p>", memory);
 
     uint8_t* mem = (uint8_t*)memory;
     uint8_t* memStart = (uint8_t*)allocator->memory;
-    printf("Mem: <%p>", mem);
-
     size_t offset = mem - memStart;
+    printf("offset: %lu\n", offset);
 
     //size_t offset = (uint8_t *)memory - (uint8_t *)allocator->memory;
-    printf("%lu", offset);
     size_t block_size = MIN_BLOCK_SIZE;
     while (block_size <= allocator->size) {
         size_t index = get_block_index(block_size, allocator->size, offset);
+        printf("Index: %lu\n", index);
         if ((allocator->bitmap[index / 8] & (1 << (7 - (index % 8)))) != 0) {
-            // Блок занят
             allocator->bitmap[index / 8] &= ~(1 << (7 - (index % 8)));
             return;
         }
-        block_size /= 2;
+        block_size *= 2;
     }
 }
 
@@ -214,26 +199,28 @@ int main() {
         ptr1[i] = i;
         printf("ptr1[%d] = %d\n", i, ptr1[i]);
     }
-    printf("hui1\n");
-    //int *ptr2 = (int*)allocator_alloc(allocator, 64);
+    //printf("hui1\n");
+    int *ptr2 = (int*)allocator_alloc(allocator, 64);
     // ptr2[0] = 91;
     // ptr2[1] = 92;
     // printf("%d - %d\n", ptr1[0], ptr1[1]);
-    printf("hui2\n");
+    //printf("hui2\n");
 
     //printBits(allocator->bitmap[0]);
     //printBits(allocator->bitmap[1]);
-    printf("ptr1: %p\n", ptr1);
-    ptr1 = (void*)ptr1;
-    printf("ptr1: %p\n", ptr1);
-    printf("ptr1: %d\n", ptr1);
-    printf("Mem start: %p\n", allocator->memory);
-    printf("Mem start: %d\n", allocator->memory);
+    //ptr1 = (void*)ptr1;
+    //printf("ptr1: %p\n", ptr1);
+    //printf("Mem start: %p\n", allocator->memory);
+
     allocator_free(allocator, ptr1);
-    printf("\n alloc freed\n");
+
     printBits(allocator->bitmap[0]);
     printBits(allocator->bitmap[1]);
-    //allocator_free(allocator, ptr2);
+    printf("\n");
+    allocator_free(allocator, ptr2);
+    printBits(allocator->bitmap[0]);
+    printBits(allocator->bitmap[1]);
+    printf("\n");
     allocator_destroy(allocator);
 
     return 0;
